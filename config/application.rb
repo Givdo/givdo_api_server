66b0ede6b38995148eb04177a6b466e5374fff34
Melody Serra <melodyserra@gmail.com>
2015-10-04 15:00:47 -0700

    added http headers for ionic

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module GivdoApiServer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Allow-Methods' => 'POST, PUT, DELETE, GET, OPTIONS',
+      'Access-Control-Request-Method' => '*',
+      'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    }
   end
 end
