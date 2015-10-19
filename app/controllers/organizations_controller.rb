8454034ae716e88868fb04c2f049c1dd48f033d3
Melody Serra <melodyserra@gmail.com>
2015-10-18 17:41:26 -0700

    Fix conflicts

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,13 +3,13 @@ require 'facebook_graph'
 include FacebookGraph
 
 class OrganizationsController < ApplicationController
-  
+
   def index
-    
+
     @organizations = Organization.all
     logger.debug "org count:" + @organizations.count.to_s
     logger.debug @organizations.to_s
-    
+
     for organization in @organizations
       if !organization.cached?
         logger.debug "reading from graph for uncached FB ID " + organization.facebook_id
@@ -17,10 +17,20 @@ class OrganizationsController < ApplicationController
       else
         logger.debug "organization " + organization['name'] + " is already cached"
       end
+
+      graph = get_graph.get_object(organization.facebook_id)
+      organization['name'] = graph['name']
+      organization['city'] = graph['location']['city']
+      organization['state'] = graph['location']['state']
+      organization['zip'] = graph['location']['zip']
+      organization['street'] = graph['location']['street']
+      organization['picture'] = get_graph.get_picture(organization.facebook_id, {type:"large"})
+      organization['mission'] = graph['mission']
+      logger.debug graph.to_s
     end
-    
+
     render json: @organizations
-    
+
   end
-        
+
 end
