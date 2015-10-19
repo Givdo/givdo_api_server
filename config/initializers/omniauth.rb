f3f4dc1c50eeb096d40aa65596299003a9a07e36
Carlos Palhares <chjunior@gmail.com>
2015-10-19 16:06:58 -0200

    Move facebook configuration to Rails secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,4 @@
-#OmniAuth.config.logger = Rails.logger
-
-FACEBOOK_CONFIG = HashWithIndifferentAccess.new(YAML.load(File.read(File.expand_path('../../facebook.yml', __FILE__))))
-
-module Facebook
-  APP_ID = FACEBOOK_CONFIG[:facebook][:app_id]
-  SECRET = FACEBOOK_CONFIG[:facebook][:secret]
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Facebook::APP_ID, Facebook::SECRET
+  credentials = Rails.application.secrets.facebook.values_at('app_id', 'secret')
+  provider :facebook, *credentials
 end
--- lib/facebook_graph.rb
+++ lib/facebook_graph.rb
@@ -1,11 +1,11 @@
 module FacebookGraph
-  
   def get_graph
-    oauth = Koala::Facebook::OAuth.new Facebook::APP_ID, Facebook::SECRET
+    credentials = Rails.application.secrets.facebook.values_at('app_id', 'secret')
+    oauth = Koala::Facebook::OAuth.new *credentials
     app_access_token = oauth.get_app_access_token
     Koala::Facebook::API.new app_access_token
   end
-  
+
   def cache_organization(organization, graph)
     ograph = graph.get_object(organization.facebook_id)
     organization['name'] = ograph['name']
@@ -18,18 +18,17 @@ module FacebookGraph
     organization['cached'] = true
     organization.save
   end
-  
+
   def cache_organizations
     graph = get_graph
     for organization in Organization.all
       if !organization.cached?
-        puts "caching organization with FB ID " + organization['facebook_id'] 
-        cache_organization(organization, graph)  
+        puts "caching organization with FB ID " + organization['facebook_id']
+        cache_organization(organization, graph)
         puts "organization " + organization['name'] + " was cached successfully!"
       else
         puts "organization " + organization.name + " is already cached"
       end
-    end    
+    end
   end
-  
 end
