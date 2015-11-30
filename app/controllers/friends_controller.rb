413c9fa09c15dde0edc51a979217a807c79d7433
Carlos Palhares <chjunior@gmail.com>
2015-11-30 21:42:09 -0200

    Increase friends list limit

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,8 +3,16 @@ require_dependency 'givdo/facebook'
 class FriendsController < ApplicationController
   before_filter :authenticate_user!
 
+  FRIENDS_PER_PAGE = 100
+
   def index
-    render json: Givdo::Facebook.invitable_friends(current_user, params),
+    render json: Givdo::Facebook.invitable_friends(current_user, friends_list_params),
            serializer: FriendsListSerializer
+  end
+
+  private
+
+  def friends_list_params
+    params.merge(limit: FRIENDS_PER_PAGE)
   end
 end
--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -2,22 +2,35 @@ require 'rails_helper'
 require 'givdo/facebook'
 
 RSpec.describe FriendsController, type: :controller do
-  let(:friends) { Givdo::Facebook::PaginatedConnections.new(nil, nil, nil) } # Any activemodel, since stubbing doesn't give you a model_name
   let(:current_user) { build(:user) }
 
   describe 'GET /friends' do
-    subject { get :index, :format => :json }
-
-    it_behaves_like 'an authenticated only action'
-
-    it 'renders the facebook invitable friends from the user' do
-      api_user(current_user)
-
+    let(:friends) { Givdo::Facebook::PaginatedConnections.new(nil, nil, nil) } # Any activemodel, since stubbing doesn't give you a model_name
+    before do
       allow(friends).to receive(:next_page_params).and_return([])
       allow(friends).to receive(:list).and_return([])
+    end
 
-      expect(Givdo::Facebook).to receive(:invitable_friends).with(current_user, anything).and_return(friends)
-      expect(subject.body).to serialize_object(friends).with(FriendsListSerializer)
+    it_behaves_like 'an authenticated only action' do
+      subject { get :index, :format => :json }
+    end
+
+    it 'renders 100 facebook invitable friends from the user' do
+      api_user(current_user)
+      expect(Givdo::Facebook).to receive(:invitable_friends).with(current_user, hash_including(:limit => 100)).and_return(friends)
+
+      get :index, :format => :json
+
+      expect(response.body).to serialize_object(friends).with(FriendsListSerializer)
+    end
+
+    it 'renders the page given by page parameter' do
+      api_user(current_user)
+      expect(Givdo::Facebook).to receive(:invitable_friends).with(current_user, hash_including(:page => ['next', {:page => 'param'}], :limit => 100)).and_return(friends)
+
+      get :index, :page => ['next', {:page => 'param'}], :format => :json
+
+      expect(response.body).to serialize_object(friends).with(FriendsListSerializer)
     end
   end
 end
