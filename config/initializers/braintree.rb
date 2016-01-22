964d097ad109456f0004699c83ed88f5bd7aa62c
Carlos Palhares <chjunior@gmail.com>
2016-01-22 21:31:34 -0200

    Fix initialization when no braintree config is set

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,6 +1,6 @@
 settings = Rails.application.secrets.braintree
 
-Braintree::Configuration.environment = settings['environment'].to_sym
+Braintree::Configuration.environment = settings['environment'].try(:to_sym)
 Braintree::Configuration.merchant_id = settings['merchant_id']
 Braintree::Configuration.private_key = settings['private_key']
 Braintree::Configuration.public_key  = settings['public_key']
