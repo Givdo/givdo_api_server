79380715b53e964f636723600302c651e25865cf
Carlos Palhares <chjunior@gmail.com>
2016-01-28 20:16:02 -0200

    Add image and name to player

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,8 +1,12 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :rounds_left, :organization, :score, :finished?, :name
+  attributes :rounds_left, :organization, :score, :finished?, :name, :image
 
   def name
     object.user.try(:name)
+  end
+
+  def image
+    object.user.try(:image)
   end
 
   def organization
--- spec/serializers/player_serializer_spec.rb
+++ spec/serializers/player_serializer_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PlayerSerializer, :type => :serializer do
   let(:organization) { build(:organization, :name => 'Save Dave') }
-  let(:player) { build(:player, :organization => organization, :finished_at => DateTime.current) }
+  let(:user) { build(:user, :name => 'John Doe', :image => 'ugly-picture.jpg') }
+  let(:player) { build(:player, :organization => organization, :user => user, :finished_at => DateTime.current) }
   before { allow(player).to receive(:rounds_left).and_return(5) }
   before { allow(player).to receive(:score).and_return(3) }
   subject { serialize(player, PlayerSerializer) }
@@ -11,4 +12,6 @@ RSpec.describe PlayerSerializer, :type => :serializer do
   it { is_expected.to serialize_attribute(:organization).with('Save Dave') }
   it { is_expected.to serialize_attribute(:score).with(3) }
   it { is_expected.to serialize_attribute(:finished?).with(true) }
+  it { is_expected.to serialize_attribute(:name).with('John Doe') }
+  it { is_expected.to serialize_attribute(:image).with('ugly-picture.jpg') }
 end
