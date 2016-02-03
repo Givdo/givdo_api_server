b6a1cf9595dcfa7e9eb99bf8d4d633affaa983fa
Carlos Palhares <chjunior@gmail.com>
2016-02-03 19:02:38 -0200

    Remove braintree

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ gem 'rails', '4.2.5'
 
 # App Logic
 gem 'koala', '~> 2.2'
-gem 'braintree'
 gem 'active_model_serializers', :github => 'rails-api/active_model_serializers'
 gem 'kaminari'
 gem 'jwt'
--- config/initializers/braintree.rb
+++
@@ -1,6 +0,0 @@
-settings = Rails.application.secrets.braintree
-
-Braintree::Configuration.environment = settings['environment'].try(:to_sym)
-Braintree::Configuration.merchant_id = settings['merchant_id']
-Braintree::Configuration.private_key = settings['private_key']
-Braintree::Configuration.public_key  = settings['public_key']
