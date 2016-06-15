92dfa725615b14d27cadf2cb84569abcae5d3734
Diego Toral <diegotoral@gmail.com>
2016-06-14 23:30:23 -0300

    Add some more fields to notification serializer

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,5 +1,20 @@
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :id, :status
+  attributes :id, :status, :category, :sender_name, :sender_image, :game_id
 
-  belongs_to :sender, serializer: UserSerializer
+
+  def category
+    object.game.try(:category).try(:name)
+  end
+
+  def sender_name
+    object.try(:sender).try(:name)
+  end
+
+  def sender_image
+    object.try(:sender).try(:image)
+  end
+
+  def game_id
+    object.try(:game).try(:id)
+  end
 end
