f2444c9d4ff2e0aab3d8e405168e80e01b1f3a9c
Diego Toral <diegotoral@gmail.com>
2016-09-08 18:39:54 -0300

    Grab user's cover from Facebook API

--- app/serializers/friend_user_serializer.rb
+++ app/serializers/friend_user_serializer.rb
@@ -1,5 +1,9 @@
 class FriendUserSerializer < ActiveModel::Serializer
   type 'users'
 
-  attributes :name, :image, :uid, :provider
+  attributes :name, :image, :cover, :uid, :provider
+
+  has_many :badges
+  has_many :causes
+  belongs_to :organization
 end
--- lib/givdo/facebook/friends.rb
+++ lib/givdo/facebook/friends.rb
@@ -2,9 +2,11 @@ require 'givdo/facebook/paginated_connections'
 
 module Givdo
   module Facebook
+    USER_FIELDS = [:name, :email, :cover]
+
     class Friends < PaginatedConnections
       def initialize(graph, params)
-        super graph, 'friends', params
+        super graph, 'friends', params.merge(fields: USER_FIELDS)
       end
 
       def users
