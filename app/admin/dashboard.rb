5cc15eb1c3633eb9593e5d9230e8ddc41e5d8aaf
Milan Shah <shahmileez@gmail.com>
2017-09-15 21:12:32 -0500

    Temporary revert back dashboard.rb to resolve issue

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,29 +12,6 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    columns do
-      column do
-        panel "Recent Posts" do
-          ul do
-            Post.recent(5).map do |post|
-              li link_to(post.title, admin_post_path(post))
-            end
-          end
-        end
-      end
-
-      column do
-        panel "Info" do
-          para "Welcome to ActiveAdmin."
-        end
-      end
-    end
-  end # content
-end
-
-# --- Back up ---- Atchi 9/14  
-  # Here is an example of a simple dashboard with columns and panels.
-    #
     # columns do
     #   column do
     #     panel "Recent Posts" do
@@ -52,3 +29,5 @@ end
     #     end
     #   end
     # end
+  end # content
+end
