9a27c4a52ae287ff494376dbf76969d7cfb09bc7
Milan Shah <shahmileez@gmail.com>
2017-09-18 18:08:18 -0500

    Initial commit for dashboard panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,42 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc {I18n.t("active_admin.dashboard")}
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: proc {I18n.t("active_admin.dashboard")} do
+    columns do
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+
+      column do
+        panel "Number of Users" do
+          users = User.all.count(:id)
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Users" do
+          table_for User.order("created_at desc").limit(5).each do |_user|
+            column(:name)
+            column(:provider)
+            column(:created_at)
+          end
+        end
+      end
+      column do
+        panel "Number of users per game" do
+          table_for Player do
+            column("Game ID=>Users") { Player.group(:game_id).distinct.count(:user_id) }
+            # panel "Number of Game Users" do
+            # value = Player.group(:game_id).distinct.count(:user_id)
+          end
+        end
+      end
+    end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
